92d9c7dabb88357b17c09c9843911c4bec453576
Arun Abraham <arunnabraham@gmail.com>
2021-10-28 04:13:24 +0530

    updated podman run

--- podman-run.sh
+++ podman-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # podman run --net slirp4netns:allow_host_loopback=true -p 8000:80 -it --rm php-apache-alpine:latest
-podman run --net slirp4netns:allow_host_loopback=true -p 8000:80 -p 4433:443 -v /home/neoxenos/work/wisilica:/var/www/localhost/htdocs:z -it --rm php-apache-alpine:latest
+podman run -d --net slirp4netns:allow_host_loopback=true --name webapp1 -p 8000:80 -p 4433:443 -v /home/neoxenos/work/wisilica:/var/www/localhost/htdocs:z -it --rm php-apache-alpine:latest
